b894248a934eb01b8416461009368cc46d751719
sesdear <orekhovs09@gmail.com>
2026-01-22 20:40:19 +0700

    Fix install,sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Installing...
 rm -rf ~/.local/share/gnome-shell/extensions/zapretswitcher-gnome@sesdear.github.io
-cp zapretswitcher-gnome@sesdear.github.io ~/.local/share/gnome-shell/extensions/
+cp -r zapretswitcher-gnome@sesdear.github.io ~/.local/share/gnome-shell/extensions/
 echo Install Success!
